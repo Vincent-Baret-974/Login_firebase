2112ab7ac5aba5212b2706aba261526492dc8fc5
vincent-baret-974 <vincentbaretcontact@gmail.com>
2024-01-08 22:04:35 +0100

    feat: add state control for password

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: LoginPage(),
     );
   }
@@ -26,10 +27,20 @@ class LoginPage extends StatelessWidget {
       body: Center(
         child: Container(
           margin: const EdgeInsets.all(20),
-          child: const Column(
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              CustomTextField()
+              CustomTextField(
+                  leadingIcon: Icons.mail_outline_rounded,
+                  placeholderText: 'Enter your email',
+                  displayVisibilityIcon: false
+              ),
+              const SizedBox(height: 20),
+              CustomTextField(
+                  leadingIcon: Icons.lock_open_rounded,
+                  placeholderText: 'Enter your password',
+                  displayVisibilityIcon: true
+              )
             ],
           ),
         ),
@@ -38,24 +49,58 @@ class LoginPage extends StatelessWidget {
   }
 }
 
-class CustomTextField extends StatelessWidget {
-  const CustomTextField({
+class CustomTextField extends StatefulWidget {
+  CustomTextField({
     super.key,
+    required this.leadingIcon,
+    required this.placeholderText,
+    required this.displayVisibilityIcon
   });
+
+  final IconData leadingIcon;
+  final String placeholderText;
+  final bool displayVisibilityIcon;
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  late IconData suffixIcon;
+  late bool obscurePassword;
+
+  @override
+  void initState() {
+    super.initState();
+    suffixIcon = Icons.visibility_off_outlined;
+    obscurePassword = widget.displayVisibilityIcon ? true : false;
+  }
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       decoration: InputDecoration(
           floatingLabelBehavior: FloatingLabelBehavior.never,
-          hintText: 'Enter your email',
+          hintText: widget.placeholderText,
           hintStyle: GoogleFonts.roboto(
             color: Colors.grey.shade400
           ),
           prefixIcon: Icon(
-              Icons.mail_outline_rounded,
+              widget.leadingIcon,
               color: Colors.grey.shade400
           ),
+          suffixIcon: widget.displayVisibilityIcon ? IconButton(
+            icon: Icon(suffixIcon),
+            color: Colors.grey.shade400,
+            onPressed: () {
+              setState(() {
+                obscurePassword = !obscurePassword;
+                suffixIcon = obscurePassword
+                    ? Icons.visibility_off_outlined
+                    : Icons.visibility_outlined;
+              });
+            },
+          ) : null,
           border: const UnderlineInputBorder(
             borderSide:
                 BorderSide.none,
@@ -64,6 +109,7 @@ class CustomTextField extends StatelessWidget {
           fillColor: Colors.grey.shade200,
           contentPadding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 0.0),
       ),
+      obscureText: obscurePassword,
       onChanged: (value) {
         print('Typed $value');
       }
